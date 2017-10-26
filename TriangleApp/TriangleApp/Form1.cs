89dae48e672b31cd38ac6295f54f0412454f8466
grzewas <32064426+grzewas@users.noreply.github.com>
2017-10-26 10:26:45 +0200

    Add files via upload

---
+++ TriangleApp/TriangleApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TriangleMath;
+
+namespace TriangleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double edgeOne = int.Parse(textBox1.Text);
+                double edgeTwo = int.Parse(textBox2.Text);
+                double edgeThree = int.Parse(textBox3.Text);
+                Triangle T = new Triangle(edgeOne, edgeTwo, edgeThree);
+                double area = T.TriangleArea();
+                double perimeter = T.TrianglePerimeter();
+                label6.Text = perimeter.ToString();
+                label7.Text = area.ToString();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+    }
+}
---
+++ TriangleApp/TriangleApp/TriangleMath.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangleMath
+{
+    class Point
+    {
+        public double coordinateX;
+        public double coordinateY;
+
+        public Point()
+        {
+            coordinateX = 1;
+            coordinateY = 1;
+        }
+
+        public Point(double coordinateX, double coordinateY)
+        {
+            this.coordinateX = coordinateX;
+            this.coordinateY = coordinateY;
+        }
+    }
+
+    class Triangle
+    {
+        double edgeOne;
+        double edgeTwo;
+        double edgeThree;
+        
+        public Triangle()
+        {
+            edgeOne = 1;
+            edgeTwo = 2;
+            edgeThree = 3;
+        }
+
+        public Triangle(double edgeOne, double edgeTwo, double edgeThree)
+        {
+            this.edgeOne = edgeOne;
+            this.edgeTwo = edgeTwo;
+            this.edgeThree = edgeThree;
+        }
+
+        public Triangle(Triangle T)
+        {
+            this.edgeOne = T.edgeOne;
+            this.edgeTwo = T.edgeTwo;
+            this.edgeThree = T.edgeThree;
+        }
+
+        public Triangle(Point vertexOne, Point vertexTwo, Point vertexThree)
+        {
+            this.edgeOne = Math.Sqrt((vertexOne.coordinateX - vertexTwo.coordinateX) * (vertexOne.coordinateX - vertexTwo.coordinateX) + (vertexOne.coordinateY - vertexTwo.coordinateY) * (vertexOne.coordinateY - vertexTwo.coordinateY));
+            this.edgeTwo = Math.Sqrt((vertexThree.coordinateX - vertexTwo.coordinateX) * (vertexThree.coordinateX - vertexTwo.coordinateX) + (vertexThree.coordinateY - vertexTwo.coordinateY) * (vertexThree.coordinateY - vertexTwo.coordinateY));
+            this.edgeThree = Math.Sqrt((vertexOne.coordinateX - vertexThree.coordinateX) * (vertexOne.coordinateX - vertexThree.coordinateX) + (vertexOne.coordinateY - vertexThree.coordinateY) * (vertexOne.coordinateY - vertexThree.coordinateY));
+        }
+
+        public double TrianglePerimeter()
+        {
+            double perimeter = edgeOne + edgeTwo + edgeThree;
+            return perimeter;
+        }
+
+        public double TriangleArea()
+        {
+            double p = (edgeOne + edgeTwo + edgeThree) / 2.0;
+            double area = Math.Sqrt(p * (p - edgeOne) * (p - edgeTwo) * (p - edgeThree));
+            return area;
+        }
+    }
+}
